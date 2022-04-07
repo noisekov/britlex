3575bf3e9b94b86196aea674ed415627a69f6fd5
Vladimir <noise123q@mail.ru>
2022-04-07 22:12:34 +0300

    burger

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,17 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from "swiper";
 
 const swiper = new Swiper();
+
+
+const menuBody = document.querySelector('.header__burger');
+document.addEventListener('click', burger);
+
+function burger(event) {
+    if (event.target.closest('.header__burger')) {
+        menuBody.classList.toggle('.active');
+    }
+    if (!event.target.closest('.header__burger')) {
+        menuBody.classList.remove('.active');
+    }
+}
+
